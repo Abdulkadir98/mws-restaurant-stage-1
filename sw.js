93bc7dccfc899a24b568427ef9b7fa92b28e8bcc
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-07-03 20:50:59 +0530

    type: saving assets to cache pn first load

--- sw.js
+++ sw.js
@@ -1,5 +1,29 @@
+self.addEventListener('install', function(event) {
+
+  let urlsToCache = [
+    '/',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'js/dbhelper.js',
+    '/css/styles.css',
+    'data/restaurants.json'
+
+  ];
+
+
+  event.waitUntil(
+    caches.open('mws-static-v1').then(function(cache) {
+        return cache.addAll(urlsToCache);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-    //console.log(event.request);
-    console.log('Hello World!');
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+    })
+  );
 
 });
